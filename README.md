8de436bf6601d0b1a2086c708d7b5e0f6399ad32
googflog <googflog@gmail.com>
2014-05-01 23:01:49 +0900

    Revert 5f6d8a9..58d9509
    This rolls back to commit 5f6d8a9ef4da6d97b28335156da6ff91b034bb05.

--- README.md
+++
@@ -1,3 +0,0 @@
-AltViewer
-=========
-Chrome機能拡張 Alt & Title ＋ Meta ビューワのデータ。
---
+++ js/popup.js
@@ -0,0 +1,82 @@
+﻿$(function(){
+
+	/*$("h1").css({opacity: 0.0})
+	$("h1").animate(
+	{opacity: 1.0},
+    {duration: 400, easing: "linear"}
+	)
+	*/
+
+	var timer_id = setInterval( function () {
+		chrome.tabs.getSelected(null, function(tab){
+			chrome.tabs.sendRequest(tab.id, {
+				contetState: "OK_Desuka"
+			}, function(response){
+				if(response.contetLoaded){
+					clearInterval( timer_id );
+					//console.log("o",response.contetLoaded);
+					fInt();
+				}else{
+					//console.log("x",response.contetLoaded);
+				}
+				
+			});
+		});
+	} , 100 );
+
+	function fInt(){
+
+		clearInterval( timer_id );
+
+		$("#Loading").fadeOut(200);
+
+		chrome.tabs.getSelected(null, function(tab){
+			chrome.tabs.sendRequest(tab.id, {
+				fromPopUp: "apper"
+			}, function(response){
+
+
+				//console.log(response.bMoveCap);
+
+				if (response.bMoveCap_a) {
+					$("#Switch").addClass("close");
+				}else{
+					$("#Switch").removeClass("close");
+				}
+
+				if (response.bMoveCap_m) {
+					$("#Switch_Title").addClass("close");
+				}else{
+					$("#Switch_Title").removeClass("close");
+				}
+				
+				$("#Switch").click(function(){
+					chrome.tabs.getSelected(null, function(tab) {
+					  	chrome.tabs.sendRequest(tab.id, {fromPopUp:"click"}, function(response) {
+					  		if(response.bMoveCap_a==true){
+					  			$("#Switch").addClass("close");
+					  		}else{
+					  			$("#Switch").removeClass("close");
+					  		}
+					  	});
+					});
+				})
+				
+				$("#Switch_Title").click(function(){
+					chrome.tabs.getSelected(null, function(tab) {
+					  	chrome.tabs.sendRequest(tab.id, {fromPopUp:"click_m"}, function(response) {
+					  		if(response.bMoveCap_m==true){
+					  			$("#Switch_Title").addClass("close");
+					  		}else{
+					  			$("#Switch_Title").removeClass("close");
+					  		}
+					  	});
+					});
+				})
+
+
+			});
+		});
+	}
+});
+
