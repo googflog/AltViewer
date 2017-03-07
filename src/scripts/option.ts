940c1f255e949e759fc52b5c8f189ed01d136978
辻本太郎 <TaroMacbookPro@Taro-no-MacBook-Pro.local>
2017-03-08 03:34:37 +0900

    chrome.storage 追加
    chrome.storage 機能追加

--- src/scripts/option.ts
+++ src/scripts/option.ts
@@ -1,19 +1,53 @@
-$(function(){
+/// <reference path="module/jquery.d.ts" />
 
-  // セーブボタンが押されたら、
-  // ローカルストレージに保存する。
-  $("#save").click(function () {
-    localStorage["message"] = $("#message").val();
-    localStorage["bgcolor"] = $("#bgcolor").val();
-  });
+class Options {
+	constructor() {
+		var this_ = this;
 
-  // オプション画面の初期値を設定する
-  if (localStorage["message"]) {
-    $("#message").val(localStorage["message"]);
-  }
-  if (localStorage["bgcolor"]) {
-    var bgcolor = localStorage["bgcolor"];
-    $("#bgcolor option[value=" + bgcolor + "]").attr("selected", true);
-  }
+		this.getStorageVal();
 
-});
+		$("[type=checkbox]").on("change", function() {
+			this_.changeVal();
+		});
+	}
+
+	getStorageVal(): void {
+		var this_: this;
+		var items: object;
+		var defaults = {
+			alt_checkbox: true,
+			title_checkbox: true,
+			size_checkbox: true,
+			path_checkbox: true,
+			extension_checkbox: true,
+			console_checkbox: true,
+			noAltList_checkbox: true,
+			altFukidashiClose_checkbox: true
+		};
+		chrome.storage.sync.get(
+			defaults,
+			function(items) {
+				console.log(items);
+				for (var name in items) {
+					$("#" + name).prop("checked", items[name]);
+				}
+			});
+	}
+	changeVal() {
+
+		var formOptions = {
+			alt_checkbox: $("#alt_checkbox").prop("checked"),
+			title_checkbox: $("#title_checkbox").prop("checked"),
+			size_checkbox: $("#size_checkbox").prop("checked"),
+			path_checkbox: $("#path_checkbox").prop("checked"),
+			extension_checkbox: $("#extension_checkbox").prop("checked"),
+			console_checkbox: $("#console_checkbox").prop("checked"),
+			noAltList_checkbox: $("#noAltList_checkbox").prop("checked"),
+			altFukidashiClose_checkbox: $("#altFukidashiClose_checkbox").prop("checked")
+		};
+		console.log(formOptions);
+		chrome.storage.sync.set(formOptions, function() { });
+	}
+}
+
+var option_instans: Options = new Options();
